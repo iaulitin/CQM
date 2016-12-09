77707cdd0aa86e7267d411ab11ec844f9de9557e
Mobilnik <iaulitin@yandex.ru>
2016-12-09 16:07:38 +0300

    Redesigned pages, cosmetic changes in the code

--- cqm-data-access/src/main/java/org/cqm/data/repositories/ReportRepository.java
+++ cqm-data-access/src/main/java/org/cqm/data/repositories/ReportRepository.java
@@ -21,15 +21,4 @@ public interface ReportRepository extends CrudRepository<Report, Integer> {
     @Query("select r from Report r where (current_date - date) < :time")
     List<Date> findFreshestReports(@Param("time") Date time);
 
-//    @Modifying
-//    @Query("UPDATE User u SET u.userRating = u.userRating + 1 WHERE u.userId = Report.userId and Report.userId = userId")
-//    public void incrementRating(@Param("userId") Integer userId);
-//
-//    @Modifying
-//    @Query("UPDATE User u SET u.userRating = u.userRating - 1 WHERE u.userId = Report.userId and Report.userId = userId")
-//    public void decrementRating(@Param("userId") Integer userId);
-//
-//    @Modifying
-//    @Query(value = "insert into Reports (reportId, userId, cafeId, load, current_date);", nativeQuery = true)
-//    public void createReport(@Param("userId") Integer userId, @Param("cafeId") Integer cafeId, @Param ("load") int load);
 }
--- cqm-web/src/main/webapp/WEB-INF/pages/index.jsp
+++ cqm-web/src/main/webapp/WEB-INF/pages/index.jsp
@@ -16,8 +16,6 @@
     <link href="<c:url value="/resources/css/bootstrap.css" />" rel="stylesheet">
     <link href="<c:url value="/resources/css/jumbotron-narrow.css" />" rel="stylesheet">
 
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
 </head>
 <body>
     <div class="container">
